9aa21d8f5e327f7d3986bc3de26b947778afa496
Kate <tsvetkova.e.i@mail.ru>
2022-12-15 00:43:28 +0300

    Ex29

---
+++ Ex29.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) и выводит на экран десятичное представление числа, записанного в СС по основанию 15
+// N: 3 [9, 4, 12] => 2097
+//{ 0, 1, 0, 0, 1, 0, 1, 0 };
+double[] a = new double[15];
+// {0,0,0,0,0,0,0,0,0,0,0,0,9,4,12};
+for (int i = 0; i < 15; i++)
+{
+    a[i] = new Random().Next(0, 14);
+    Console.Write($"|{a[i]}|");
+
+}
+Console.WriteLine();
+double dc = 0;
+//int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 14; i >= 0; i--)
+{
+    Console.Write($"{a[i]}");
+    double degree = Math.Pow(15, 14 - i);
+    dc += (a[i] * degree);
+}
+Console.WriteLine();
+Console.Write($"{dc}");
+
