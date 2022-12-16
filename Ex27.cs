213944e1f4a5809f451123216b068ab4f3b6d548
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-17 02:55:57 +0300

    Add files via upload

---
+++ Ex27.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Строки использовать нельззя
+int Sum = 0;
+// задали размер массива
+Console.WriteLine("число равное размеру массива");
+int N = int.Parse(Console.ReadLine());
+int[] Array = new int[N];
+//через цикл присваиваем значения ячейкам массива
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"Напишите число по {i} индексу");
+    int readnumber = int.Parse(Console.ReadLine());
+    Array[i] = readnumber;
+    //вывод массива. через строку
+    // Console.WriteLine($"Index: {i} = {readnumber}");
+}
+// foreach (int i in Array)
+{
+    foreach (int i in Array) ;
+}
+for (int i = 0; i < N; i++)
+{
+    // Console.Write($"{i}");
+    //Console.Write(Array[i]);
+    Sum += Array[i];
+}
+Console.WriteLine();
+Console.WriteLine(Sum);
+//Array = int.Parse(Console.ReadLine());
