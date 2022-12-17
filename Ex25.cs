294569e12b19a0d71bc53db00d29dbe6b4f62ff8
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-17 09:03:00 +0300

    Add files via upload

---
+++ Ex25.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ Math.Pow
+
+// задали размер массива
+Console.WriteLine("число A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("число B");
+int B = int.Parse(Console.ReadLine());
+//number =A^B
+int number=1;
+for (int i=0; i<B; i++)
+{
+    number*= A;
+}
+Console.WriteLine($"число A в натуральной степени B:{number}");
